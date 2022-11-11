4b4e26c259f3c48d81a778b824751965c782775c
majikyouser <sahiagb@gmail.com>
2022-11-11 11:18:14 -0500

    reset db

--- reset_db.py
+++ reset_db.py
@@ -1,3 +1,4 @@
 from app import db, files
+
 with open("./files/README_(Do_not_delete_please).txt", "rb") as file:
   file = files(file_name = "README_(Do_not_delete_please).txt", file_blob = file.read())
